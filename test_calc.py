5eff402b78cb807fec8f1ade7745929d79e727b1
vikafrostb10 <58080864+vikafrostb10@users.noreply.github.com>
2020-12-18 14:37:45 +0200

    Update test_calc.py

--- test_calc.py
+++ test_calc.py
@@ -6,8 +6,10 @@ class TestCalc(unittest.TestCase):
 
     def test_calculate_whole_week(self):
         self.assertEqual(calc.calculate_whole_week(2014, 'April', 'May', 'Wednesday'), 5)
+        
 
 if __name__ == '__main__':
+    print("Hello")
     import xmlrunner
     runner = xmlrunner.XMLTestRunner(output='test-reports')
     unittest.main(testRunner=runner)
